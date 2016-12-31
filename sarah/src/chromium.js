69fef8436eaa901d706e9b7f73dfe36d3e37ef80
hoge <fuga@email.hoge>
2016-12-31 20:21:13 +0900

    unused but modified for safety, eslint

--- sarah/src/chromium.js
+++ sarah/src/chromium.js
@@ -1,7 +1,7 @@
 const fs     = require( 'fs' );
 const path   = require( 'path' );
 const webkit = require( './chromium.webkit.json' );
-const prefPath = path.resolve( process.env['HOME'], '.config/chromium/Default/Preferences' );
+const prefPath = path.resolve( process.env.HOME, '.config/chromium/Default/Preferences' );
 const pref   = JSON.parse(
   fs.readFileSync( prefPath )
   .toString()
@@ -9,6 +9,6 @@ const pref   = JSON.parse(
 
 pref.webkit = webkit;
 
-fs.writeFileSync( prefPath, JSON.stringify( pref, null, '' ) );
-fs.writeFileSync( '/tmp/chromium.json', JSON.stringify( pref, null, '  ' ) );
+//fs.writeFileSync( prefPath, JSON.stringify( pref, null, '' ) );
+//fs.writeFileSync( '/tmp/chromium.json', JSON.stringify( pref, null, '  ' ) );
 
