a5cdf742ce8cce19bfcf02659e54d8475256b0b8
someuser <someuser@example.com>
2017-08-03 16:20:39 +0900

    kali dev update

--- kali/ansible.sh
+++ kali/ansible.sh
@@ -1,3 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "kali called"
+while getopts "e" OPT ; do
+  case $OPT in
+    e)  echo Entering Examinational mode
+        ansible-playbook exam.yml -kK --connection=local
+        ;;
+  esac
+done
+
+if [ $# -eq 0 ] ; then
+  echo Entering Normal mode
+  ansible-playbook install.yml -kK --connection=local
+fi
+
