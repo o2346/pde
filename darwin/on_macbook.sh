b170568045e9e9b945c83f0186ccd10e72d9cfba
user <magi@magi-desktop>
2018-11-03 10:28:10 +0900

    updae

--- darwin/on_macbook.sh
+++ darwin/on_macbook.sh
@@ -8,6 +8,5 @@ cd `mktemp -d`
 echo -e "\n# applespi\napplespi\nspi_pxa2xx_platform\nspi_pxa2xx_pci" >> /etc/initramfs-tools/modules
 
 sudo apt install -y dkms
-rm -rf /usr/src/applespi-0.1
 git clone https://github.com/cb22/macbook12-spi-driver.git /usr/src/applespi-0.1
 dkms install -m applespi -v 0.1
