bb54ee74cf6fa602630e5758c1d295cee0e348de
user <official@magi-desktop>
2018-11-22 18:56:46 +0900

    add

---
+++ mint/on_mbp.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt install -y git
+sudo apt install -y dkms
+sudo apt install -y build-essential
+git clone https://github.com/Dunedan/mbp-2016-linux.git
+cd mbp-2016-linux
+sudo git clone https://github.com/cb22/macbook12-spi-driver.git /usr/src/applespi-0.1
+sudo dkms install -m applespi -v 0.1
+sudo echo -e "\n# applespi\napplespi\nintel_lpss_pci\nspi_pxa2xx_platform" >> /etc/initramfs-tools/modules
+sudo update-initramfs -u
+gcc touchbar.c
+#https://unix.stackexchange.com/questions/108888/how-creation-of-new-usb-device-file-in-dev-bus-usb-001-directory-work
+sudo ./a.out -d /dev/bus/usb/001/006 1
