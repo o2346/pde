5852092aa1239010fd1ba12dcb6aa8fd2b5f1a25
user <magi@magi-desktop>
2020-04-25 19:05:45 +0900

    add griffin powermate driver installation & activation

---
+++ mint/misc/powermate.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#https://github.com/stefansundin/powermate-linux
+#http://wirefall.dyndns.org/powermate.html
+#https://www.amazon.co.jp/dp/B003VWU2WA/ref=cm_sw_r_tw_dp_U_x_tiaPEb37ZY7EZ
+#https://www.google.com/search?q=griffin+technology+na16029+powermate+usb+multimedia+controller+linux
+
+# Register this script to like 'Startup Application' on your system
+
+cd /var/tmp
+
+if ! ls powermate-linux; then
+  git clone --recurse-submodules https://github.com/stefansundin/powermate-linux.git
+fi
+cd powermate-linux
+if [ -f "./powermate" ]; then
+  :
+else
+  sudo cp 60-powermate.rules /etc/udev/rules.d/
+  sudo apt-get install -y libpulse-dev libnotify-dev
+  sudo apt-get update --fix-missing
+  make
+fi
+
+if ps aux | grep "powermate \-d"; then
+  echo "It's already running. Abort" >&2
+else
+  ./powermate -d
+fi
