8c137f52502d97a721f652f0399ede8deaf5cbf2
o2346 <o2346@hotmail.com>
2020-12-18 18:26:39 +0900

    update

--- mint/bootstrap.sh
+++ mint/bootstrap.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-
+# curl https://raw.githubusercontent.com/o2346/pde/develop/mint/bootstrap.sh | bash -s
 sudo su
 apt install -y openssh-server
 systemctl enable ssh.service
 sshconf=/etc/ssh/sshd_config
 permit_empty='PermitEmptyPasswords yes'
 permit_root='PermitRootLogin yes'
-if ! cat $sshconf | grep "$permit_empty"; then
+if [ "`cat $sshconf | grep '$permit_empty'`" = "" ] ; then
   echo "$permit_empty"  >> $sshconf
 fi
-if ! cat $sshconf | grep "$permit_root"; then
+if [ "`cat $sshconf | grep '$permit_root'`" = "" ] ; then
   echo "$permit_root"  >> $sshconf
 fi
 
