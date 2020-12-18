749c88f42c2b2043d9d1341ada011751f22f384f
o2346 <o2346@hotmail.com>
2020-12-18 18:15:20 +0900

    idempotency

--- mint/bootstrap.sh
+++ mint/bootstrap.sh
@@ -3,7 +3,15 @@
 sudo su
 apt install -y openssh-server
 systemctl enable ssh.service
-echo 'PermitEmptyPasswords yes' >> /etc/ssh/sshd_config
-echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config
+sshconf=/etc/ssh/sshd_config
+permit_empty='PermitEmptyPasswords yes'
+permit_root='PermitRootLogin yes'
+if ! cat $sshconf | grep $permit_empty; then
+  echo "$permit_empty"  >> $sshconf
+fi
+if ! cat $sshconf | grep $permit_root; then
+  echo "$permit_root"  >> $sshconf
+fi
+
 sed -i 's|UsePAM yes|UsePam no|' /etc/ssh/sshd_config
 systemctl restart ssh.service
