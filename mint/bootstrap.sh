721ee61726515bbc4f103927a6166e242385470e
o2346 <o2346@hotmail.com>
2020-12-18 18:17:21 +0900

    bug

--- mint/bootstrap.sh
+++ mint/bootstrap.sh
@@ -6,10 +6,10 @@ systemctl enable ssh.service
 sshconf=/etc/ssh/sshd_config
 permit_empty='PermitEmptyPasswords yes'
 permit_root='PermitRootLogin yes'
-if ! cat $sshconf | grep $permit_empty; then
+if ! cat $sshconf | grep "$permit_empty"; then
   echo "$permit_empty"  >> $sshconf
 fi
-if ! cat $sshconf | grep $permit_root; then
+if ! cat $sshconf | grep "$permit_root"; then
   echo "$permit_root"  >> $sshconf
 fi
 
