3005481cf323428f2c82c5f2a537a82366a6dd64
o2 <o2346@hotmail.com>
2021-01-08 12:26:41 +0900

    it still works

--- debian/partialize.sh
+++ debian/partialize.sh
@@ -4,9 +4,18 @@ cd $(dirname $(realpath $0))
 
 declare -r fundamental='fundamental'
 declare -r partial=$1
-
+declare -r fromto='('$(echo "$fundamental $*" | tr ' ' '|')')'
+echo $fromto >&2
+#\documentclass{custom-partializer-incompatible}
+#test: git ls-tree --name-only HEAD | ciwl 'cat install.yml | ./partialize.sh test'
 #https://stackoverflow.com/a/3717989
-# example: cat install.yml | awk '
+#https://stackoverflow.com/questions/3717772/regex-grep-for-multi-line-search-needed
+# example: cat install.yml | awk '/\\begin{(test|exam)}/,/\\end{(test|exam)}/'
+#https://en.wikibooks.org/wiki/LaTeX/Basics
 cat -                 |
-awk '/\\begin{(test|exam)}/,/\\end{(test|exam)}/'
+grep  -Pzo '(?s).*\\documentclass{custom-partializer-incompatible}.*' |
+awk '/\\begin{'$fromto'}/,/\\end{'$fromto'}/'
+
+exit "`echo "${PIPESTATUS[*]}" | tr ' ' '+' | bc`"
+
 
