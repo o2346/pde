1c91d811d3cd4417fc39e74c0de80f6909fff7aa
o2 <o2346@hotmail.com>
2021-01-08 11:38:28 +0900

    it\'s working

--- debian/partialize.sh
+++ debian/partialize.sh
@@ -4,11 +4,9 @@ cd $(dirname $(realpath $0))
 
 declare -r fundamental='fundamental'
 declare -r partial=$1
-declare -r =$1
 
 #https://stackoverflow.com/a/3717989
-# example: cat install.yml | awk '/\\begin{(test|exam)}/,/\\end{(test|exam)}/'
+# example: cat install.yml | awk '
 cat -                 |
-awk '/select/,/from/' |
-awk '/select/,/from/'
+awk '/\\begin{(test|exam)}/,/\\end{(test|exam)}/'
 
