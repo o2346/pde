2d1ad74ee18ca427d17885ed52ad3e4a8561ee3f
o2 <o2346@hotmail.com>
2021-05-28 07:31:33 +0900

    add script for 9310 kali

---
+++ debian/xps13_9310/initiate_wifi.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#https://torlenor.org/linux/2020/10/31/mint_on_dell_xps_13.html
+printf '
+deb http://ppa.launchpad.net/somerville-dla-team/ppa/ubuntu bionic main
+# deb-src http://ppa.launchpad.net/somerville-dla-team/ppa/ubuntu bionic main
+# deb-src http://ppa.launchpad.net/somerville-dla-team/ppa/ubuntu bionic main
+' > /etc/apt/sources.list.d/somerville-dla-team-ubuntu-ppa-bionic.list
+
+printf '
+deb http://oem.archive.canonical.com/ focal oem
+#deb-src http://oem.archive.canonical.com/ focal oem
+' > /etc/apt/sources.list.d/focal-oem.list
+
+printf '
+deb http://dell.archive.canonical.com/ focal somerville
+# deb-src http://dell.archive.canonical.com/ focal somerville
+deb http://dell.archive.canonical.com/ focal somerville-bulbasaur
+# deb-src http://dell.archive.canonical.com/ focal somerville-bulbasaur
+' > /etc/apt/sources.list.d/oem-somerville-bulbasaur-meta.list
+
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys F992900E3BBF9275 F9FDA6BED73CDC22 F9FDA6BED73CDC22 78BD65473CB3BD13
+sudo apt-get update
+#sudo apt-get dist-upgrade
+sudo apt-get install ubuntu-oem-keyring oem-somerville-meta oem-somerville-bulbasaur-meta
+
+#https://gist.github.com/gwillem/e14fe0eedf7625c31116ae0163943e2e
