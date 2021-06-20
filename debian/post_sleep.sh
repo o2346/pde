3fd5a7f1b1b0377a184d0eef3acd53ba72d21624
o2 <o2346@hotmail.com>
2021-06-20 17:57:29 +0900

    every wakeup, not limited to resume from suspend

--- debian/post_sleep.sh
+++ debian/post_sleep.sh
@@ -10,8 +10,5 @@ fi
 if [ "${1}" == "post" ]; then
   date > /tmp/postsuspend.log
   uuidgen >> /tmp/postsuspend.log
-  #https://opensource.com/article/18/11/how-swap-ctrl-and-caps-lock-your-keyboard
-  /usr/bin/setxkbmap -option "ctrl:nocaps"
-  #https://askubuntu.com/questions/271387/how-to-restart-wifi-connection/271739#271739
-  sudo ifconfig wlan0 up
+  /opt/onwakeup/onwakeup.sh
 fi
