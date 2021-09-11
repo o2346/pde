14d96032370801cd8e9013ec746a1c463589d967
o2 <o2346@hotmail.com>
2021-09-11 10:42:18 +0900

    without requireing sudo

--- debian/devices/trackball/trackballw.sh
+++ debian/devices/trackball/trackballw.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
-tail -f /var/log/syslog | while read f; do
-  if echo $f | grep "input: Kensington Kensington Slimblade Trackball" > /dev/null; then
+#No use for systemd, unable to connect X server
+#tail -f /var/log/syslog | while read f; do
+
+#without requireing sudo
+#https://askubuntu.com/a/166438
+udevadm monitor --udev | while read f; do
+  bus_no=`lsusb | grep 'Kensington SlimBlade Trackball' | awk '{print $2}' | bc`
+  if echo $f | grep "usb${bus_no}" > /dev/null; then
     echo "Trackball Connected!!"
-    /usr/bin/xinput set-button-map "Kensington Kensington Slimblade Trackball" 3 3 1 5 4 6 7 1 9 10 11 12 | logger
-    /usr/bin/xinput  --set-prop  "Kensington Kensington Slimblade Trackball" "Device Accel Constant Deceleration" 4 | logger
-    sleep 1
+    while ! /usr/bin/xinput list 'Kensington Kensington Slimblade Trackball'; do
+      sleep 1
+      echo "still waiting.."
+    done
+    echo "setting up trackball"
+    /usr/bin/xinput set-button-map "Kensington Kensington Slimblade Trackball" 3 3 1 5 4 6 7 1 9 10 11 12
+    /usr/bin/xinput  --set-prop  "Kensington Kensington Slimblade Trackball" "libinput Accel Speed" -1
   fi
+  sleep 1
 done
+
