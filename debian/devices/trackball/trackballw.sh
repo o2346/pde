a766aeb9943c193a7ac0294fbe478243cf95ac1a
o2 <o2346@hotmail.com>
2021-09-11 11:11:49 +0900

    Report only when everything went fine

--- debian/devices/trackball/trackballw.sh
+++ debian/devices/trackball/trackballw.sh
@@ -7,14 +7,14 @@
 udevadm monitor --udev | while read f; do
   bus_no=`lsusb | grep 'Kensington SlimBlade Trackball' | awk '{print $2}' | bc`
   if echo $f | grep "usb${bus_no}" > /dev/null; then
-    echo "Trackball Connected!!"
+    echo "Trackball might be Connected!!"
     while ! /usr/bin/xinput list 'Kensington Kensington Slimblade Trackball'; do
       sleep 1
       echo "still waiting.."
     done
-    echo "setting up trackball"
-    /usr/bin/xinput set-button-map "Kensington Kensington Slimblade Trackball" 3 3 1 5 4 6 7 1 9 10 11 12
-    /usr/bin/xinput  --set-prop  "Kensington Kensington Slimblade Trackball" "libinput Accel Speed" -1
+    /usr/bin/xinput set-button-map "Kensington Kensington Slimblade Trackball" 3 3 1 5 4 6 7 1 9 10 11 12 &&
+    /usr/bin/xinput  --set-prop  "Kensington Kensington Slimblade Trackball" "libinput Accel Speed" -1    &&
+    echo "Trackball Configured"
   fi
   sleep 1
 done
